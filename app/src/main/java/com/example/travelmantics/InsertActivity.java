abe61addb2d133d467621ffd6f04101c2c10d0db
Joshua Folivi <joshua.folivi@gmail.com>
2019-08-07 01:36:21 +0100

    Writing To A Firebase Database

--- app/src/main/java/com/example/travelmantics/InsertActivity.java
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -2,14 +2,74 @@ package com.example.travelmantics;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class InsertActivity extends AppCompatActivity {
+    //fields declaration
+    private FirebaseDatabase _firebaseDatabase;
+    private DatabaseReference _databaseReference;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+        _firebaseDatabase = FirebaseDatabase.getInstance();
+        _databaseReference = _firebaseDatabase.getReference().child("traveldeals");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal Saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        //use database reference to push saved items to database
+        _databaseReference.push().setValue(deal);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtPrice.setText("");
+
+        txtTitle.requestFocus();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
